c6ba64e4dfa0c453cb752858681e1e83931efd4e
DmitriyHoloven <goloven.dmitriy@gmail.com>
2022-04-12 21:46:24 +0300

    wabPack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import './style/index.scss';
+
+const hit = document.getElementsByClassName('img')
+console.log(hit)
+hit.textContent = '<img class="img" src="./img/koloda.png" alt="koloda kart">'
 
 class Game {
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
 const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js'},
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'BlackJack',
+            template: './src/index.html'
+        })
+    ],
     module: {
         rules: [
             {
-                test: /\.(s*)css$/,
-                use: [ 'style-loader', 'css-loader','sass-loader' ] },
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
             },
             {
+
                 test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'img',
+                    esModule: false,
+                },
             },
         ],
     },
